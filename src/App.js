5ecda07e75ebb15770a94d3cf104d50c6ef4bac3
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2021-04-12 07:02:24 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items)
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+
+        <div className='section-center'>
+          {menu.map((menu) => {
+            return <Menu key={menu.id} {...menu} />
+          })}
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from 'react'
 
-const Menu = () => {
-  return <h2>menu component</h2>;
-};
+const Menu = ({ id, title, category, price, img, desc }) => {
+  console.log(id, title, category, price, img, desc)
+  return (
+    <div className='section-center'>
+      <article key={id} className='menu-item'>
+        <img src={img} alt={title} className='photo' />
+        <div className='item-info'>
+          <header>
+            <h4>{title}</h4>
+            <h4 className='price'>{price}</h4>
+          </header>
+          <p className='item-text'>{desc}</p>
+        </div>
+      </article>
+    </div>
+  )
+}
 
-export default Menu;
+export default Menu
