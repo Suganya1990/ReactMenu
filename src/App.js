22471fc955d1c6e184775829685b134e40ef4496
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2021-04-12 14:34:13 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { useState } from 'react'
 import Menu from './Menu'
 import Categories from './Categories'
 import items from './data'
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 function App() {
   const [menu, setMenu] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') return setMenu(items)
+    const newItems = items.filter((item) => {
+      return item.category === category
+    })
+    setMenu(newItems)
+  }
 
   return (
     <main>
@@ -13,7 +23,7 @@ function App() {
           <h2>our menu</h2>
           <div className='underline'></div>
         </div>
-
+        <Categories categories={categories} filterItems={filterItems} />
         <div className='section-center'>
           {menu.map((menu) => {
             return <Menu key={menu.id} {...menu} />
--- src/Menu.js
+++ src/Menu.js
@@ -1,20 +1,17 @@
 import React from 'react'
 
 const Menu = ({ id, title, category, price, img, desc }) => {
-  console.log(id, title, category, price, img, desc)
   return (
-    <div className='section-center'>
-      <article key={id} className='menu-item'>
-        <img src={img} alt={title} className='photo' />
-        <div className='item-info'>
-          <header>
-            <h4>{title}</h4>
-            <h4 className='price'>{price}</h4>
-          </header>
-          <p className='item-text'>{desc}</p>
-        </div>
-      </article>
-    </div>
+    <article key={id} className='menu-item'>
+      <img src={img} alt={title} className='photo' />
+      <div className='item-info'>
+        <header>
+          <h4>{title}</h4>
+          <h4 className='price'>{price}</h4>
+        </header>
+        <p className='item-text'>{desc}</p>
+      </div>
+    </article>
   )
 }
 
